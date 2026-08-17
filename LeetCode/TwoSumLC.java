660a99b9317d17e27a8d35694e727d1c0a3b9a54
soumi2004 <soumilimallick2004@gmail.com>
2026-08-17 19:41:38 +0530

    Add Two Sum and Contains Duplicate

---
+++ LeetCode/TwoSumLC.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+import java.util.HashMap;
+
+class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+
+            int current = nums[i];
+            int needed = target - current;
+
+            if(map.containsKey(needed)) {
+                return new int[]{map.get(needed), i};
+            }
+
+            map.put(current, i);
+        }
+
+        return new int[]{};
+    }
+}
