90f1f68ff628e92e9ef2e40b9bc725be7d697c91
soumi2004 <soumilimallick2004@gmail.com>
2026-08-23 12:55:06 +0530

    add this two new programme

---
+++ LeetCode/RotateArray.java
@@ -0,0 +1,41 @@
+package LeetCode;
+
+public class RotateArray {
+
+    public static void rotate(int[] nums, int k) {
+
+        k = k % nums.length; // remove unnecessary full rotations.
+
+        reverse(nums, 0, nums.length - 1); // Reverse the entire array.
+
+        reverse(nums, 0, k - 1); // Reverse the first k elements.
+
+        reverse(nums, k, nums.length - 1); // Reverse everything after the first k elements.
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+
+        while (start < end) {
+
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+
+        int k = 3;
+
+        rotate(nums, k);
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
