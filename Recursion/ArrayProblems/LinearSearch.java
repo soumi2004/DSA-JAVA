ccf5ed2bbc55d4ead532c42b2d1b59b2679468f6
soumi2004 <soumilimallick2004@gmail.com>
2026-09-22 17:04:25 +0530

    add this

---
+++ Recursion/ArrayProblems/LinearSearch.java
@@ -0,0 +1,14 @@
+package Recursion.ArrayProblems;
+
+import java.util.Scanner;
+
+public class LinearSearch {
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+
+    }
+}
